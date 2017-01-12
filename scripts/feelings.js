bd79225a4b5faa952be6f58db3d85dfb799258e0
LeeS <leejsitu@gmail.com>
2017-01-11 21:32:24 -0500

    Rearranging labels. Added styled radio buttons. Added styled select input.

--- scripts/feelings.js
+++ scripts/feelings.js
@@ -10,6 +10,47 @@
             //height of window
             var winHeight = $(window).innerHeight();
 
+            $("p").css({
+                "margin-top": "50px",
+                "display": "inline-block"
+            });
+
+            $(".questions").css({
+                "position": "relative",
+                "overflow": "auto"
+            });
+
+            //question 1
+            $("input[type=radio]").hide();
+
+            $(".label-radio").css({
+                "position": "relative"
+            });
+
+            $(".label-radio--button").css({
+                "display": "inline-block",
+                "width": "20px",
+                "height": "1em",
+                "outline": "1px solid black",
+                "position": "relative",
+                "background-color": "red"
+            });
+
+            $(".label-radio--button---selector").css({
+                "display": "inline-block",
+                "width": "50%",
+                "height": "100%",
+                "outline": "1px solid green",
+                "background-color": "grey",
+                "float": "left"
+            });
+
+            //question 2
+            $("#select-committment").css({
+                "border": "none",
+                "border-bottom": "1px solid black"
+            });
+            //question 3
             $("#questions-container").css({
                 "position": "relative",
                 "margin": "0 auto",
@@ -71,6 +112,27 @@
                 $("html, body").animate({
                     scrollTop: $(targetQuestion).offset().top
                 }, 500);
+            });
+
+            $(".label-radio").click(function(){
+                var radioId = $(this).attr("for");
+                document.getElementById(radioId).checked = true;
+                //console.log(document.getElementById(radioId));
+                if(document.getElementById(radioId).checked){
+                    console.log("associated id is checked");
+                    //float all label-radio--button--selectors to the left
+                    $(".label-radio--button---selector").css({
+                        "float": "left",
+                        "background-color": "grey"
+                    });
+                    $(this).find(".label-radio--button .label-radio--button---selector").css({
+                        "float": "right",
+                        "background-color": "green"
+                    });
+                }
+                else{
+                    console.log("associated id is not checked");
+                }
             });
         });
     }
